747d64b9339d9c4b2f030bb09e6dd42b17fdc9dc
unknown <irfanulislamfahim@gmail.com>
2022-05-25 02:09:40 +0600

    final-commit

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -17,17 +17,17 @@ const Blogs = () => {
                                 <Accordion.Header> <h5>1. What is the difference between  Javascript and Nodejs?</h5></Accordion.Header>
                                 <Accordion.Body >
                                     <p><strong>Javascript: </strong></p>
-                                    Javascript is a scripted programming language which can run in any browser. Chrome uses its V8 engine to run javascript's code. It can run in any web browser with a proper browser engine. JavaScript is mainly used for any client-side activity for a web application.
+                                    Javascript is a scripted programming language that can run in any browser. Chrome uses its V8 engine to run javascript code. It can run in any web browser with a proper browser engine. JavaScript is mainly used for any client-side activity for a web application.
                                     Javascript is used in frontend development.
-                                    <p className='mt-3'><strong> Nodejs: </strong></p> Node JS is an interpreter or running environment for a JavaScript programming language. We can run Javascript outside the browser with the help of Node JS.Node JS can only run in a V8 engine which is mainly used by google chrome. Nodejs is used in server-side development. V8 is the Javascript engine inside of node.js that runs Javascript.
+                                    <p className='mt-3'><strong> Nodejs: </strong></p> Node JS is an interpreter or running environment for a JavaScript programming language. We can run Javascript outside the browser with the help of Node JS. Node JS can only run in a V8 engine which is mainly used by google chrome. Nodejs is used in server-side development. V8 is the Javascript engine inside of node.js that runs Javascript.
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="1">
                                 <Accordion.Header><h5>  2. When should you use Nodejs and when should you use Mongodb?</h5></Accordion.Header>
                                 <Accordion.Body>
                                     {/* <p><strong>Mongodb: </strong></p> */}
-                                    MonogDB is a database system which gives us a chance to efficiently store documents in a database and to perform operations like data update, delete or to search documents by some criterias. We primarily use Mongodb to store data.
-                                    Nodejs is used in server-side development. NodeJs recieves the request from the client side and completes the operation.Node.js can add, delete, modify data in our database. NodeJs is a server which connects the client side with the database.
+                                    MongoDB is a database system that gives us a chance to efficiently store documents in a database and to perform operations like data updates, deleting, or searching documents by some criteria. We primarily use MongoDB to store data.
+                                    Nodejs is used in server-side development. NodeJs receives the request from the client side and completes the operation.Node.js can add, delete and modify data in our database. NodeJs is a server that connects the client side with the database.
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="2">
